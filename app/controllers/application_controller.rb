53a78b5f09b61dfbd58f78cef23f620c481bcad9
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2017-11-10 22:57:26 -0200

    create account sign up (API)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include ActionController::ImplicitRender
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from ActiveRecord::RecordNotFound, Mongoid::Errors::DocumentNotFound,
               with: :record_not_found
@@ -14,5 +15,9 @@ class ApplicationController < ActionController::API
     }
     render json: payload, status: :not_found
     Rails.logger.debug exception.message
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
---
+++ spec/requests/auth_apis_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe 'Authentication Api', type: :request do
+  include_context 'db_cleanup_each', :transaction
+  let(:user_props) { FactoryGirl.attributes_for(:user) }
+
+  context 'sign-up' do
+    context 'valid registration' do
+      it 'successfully creates account' do
+        jpost user_registration_path, user_props
+        expect(response).to have_http_status :ok
+
+        payload = parsed_body
+        expect(payload).to include('status' => 'success')
+        expect(payload).to include('data')
+        expect(payload['data']).to include('id')
+        expect(payload['data']).to include('provider' => 'email')
+        expect(payload['data']).to include('uid' => user_props[:email])
+        expect(payload['data']).to include('name' => user_props[:name])
+        expect(payload['data']).to include('email' => user_props[:email])
+        expect(payload['data']).to include('created_at', 'updated_at')
+      end
+    end
+    context 'invalid registration' do
+      context 'missing information' do
+        it 'reports error with messages'
+      end
+      context 'non-unique information' do
+        it 'reports non-unique e-mail'
+      end
+    end
+  end
+
+  context 'anonymous user' do
+    it 'accesses unproteced'
+    it 'fails to access protected resource'
+  end
+
+  context 'login' do
+    context 'valid user login' do
+      it 'generates access token'
+      it 'grantes access to resource'
+      it 'grantes access to resource multiple times'
+      it 'logout'
+    end
+    context 'invalid password' do
+      it 'rejects credentials'
+    end
+  end
+end
