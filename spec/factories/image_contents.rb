574fe93ae3cbc21da411548268d87478cc5f88e5
Lucas De Castro Oliveira <lucas.co@bolsista.pti.org.br>
2017-12-21 16:17:14 -0200

    Server: Image Cropping and Marking

--- spec/factories/image_contents.rb
+++ spec/factories/image_contents.rb
@@ -1,7 +1,13 @@
 FactoryGirl.define do
   factory :image_content do
     content_type 'image/jpg'
-    content { File.open('db/images/sample.jpg', 'rb') { |f| Base64.encode64(f.read) } }
+    sequence(:content) do |idx|
+      File.open('db/images/sample.jpg', 'rb') do |f|
+        image = StringIO.new(f.read)
+        image = ImageContentCreator.annotate(idx, image)
+        Base64.encode64(image)
+      end
+    end
     original false
   end
 end
