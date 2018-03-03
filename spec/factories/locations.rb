69261fb356c360538bf09c6b1a220d06c7da6f6b
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2018-03-03 18:26:09 -0300

    DB PostalAddress and Location Value Objects

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -10,4 +10,38 @@ FactoryGirl.define do
       lat 39.3304957
     end
   end
+
+  factory :postal_address do
+    transient do
+      sequence(:street_address) {|idx| "#{3000+idx} North Charles Street"}
+      city           "Baltimore"
+      state_code     "MD"
+      zip            "21218"
+      country_code   "US"
+    end
+    initialize_with { PostalAddress.new(street_address,city,state_code,zip,country_code) }
+
+    trait :jhu do
+      street_address "3400 North Charles Street"
+      city           "Baltimore"
+      state_code     "MD"
+      zip            "21218"
+      country_code   "US"
+    end
+  end
+
+  factory :location do
+    address           { FactoryGirl.build(:postal_address) }
+    position          { FactoryGirl.build(:point) }
+    formatted_address {
+      street_no=address.street_address.match(/^(\d+)/)[1]
+      "#{street_no} N Charles St, Baltimore, MD 21218, USA"
+    }
+    initialize_with { Location.new(formatted_address,position,address) }
+
+    trait :jhu do
+      address { FactoryGirl.build(:postal_address, :jhu) }
+      position { FactoryGirl.build(:point, :jhu) }
+    end
+  end
 end
